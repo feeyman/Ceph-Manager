e6291616ecc826cd3bbe5eef49d56403f892c7a1
linzhou <443619637@qq.com>
2019-07-21 19:21:57 +0800

    test is ok
    能够显示每个osd所占用的虚拟内存和实际内存

--- Ceph-OSD/mem_per_osd.py
+++ Ceph-OSD/mem_per_osd.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python
 # -*- coding: UTF-8 -*-
 import os
 import sys
@@ -23,14 +24,19 @@ def printosdmemtable(chosse):
         row.field_names = memlist
         for root, dirs, files in os.walk('/var/run/ceph/'):
             for name in files:
-                if "osd"  in name and "pid" in name :
+                if "osd"  in name and "asok" in name :
                     osdlist = []
                     osdthlist=[]
                     for osdmem in range(len(memchose)):
                         osdlist.append(" ")
+
                     pidfile=root+ name
                     osdid=commands.getoutput('ls  %s|cut -d "." -f 2 2>/dev/null'  %pidfile )
-                    osdpid = commands.getoutput('cat %s  2>/dev/null' %pidfile)
+                  #  osdpid = commands.getoutput('cat %s  2>/dev/null' %pidfile)
+                  #  usedPercents=commands.getoutput("df -h|grep sda|awk '{print $5}'|grep -Eo '[0-9]+'").split('\n')
+
+                    osdpid = commands.getoutput("netstat -nlp|grep %s |awk '{print $9}'" %pidfile).split("/")[0]
+
                     osd_runmemvsz = commands.getoutput('ps -p %s  -o vsz |grep -v VSZ 2>/dev/null' %osdpid)
                     osd_runmemrsz = commands.getoutput('ps -p %s  -o rsz |grep -v RSZ 2>/dev/null' %osdpid)
                     osdname="osd."+osdid
